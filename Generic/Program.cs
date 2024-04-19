f7ed6f757b3b0392486b17d1188dcfd2ab7422fd
IlyaKotenko <146016199+SazanOne@users.noreply.github.com>
2024-04-19 18:05:18 +0300

    Update Program.cs

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -1,4 +1,4 @@
-﻿using Generic;
+using Generic;
 
 ComparablePair<int, string> pair1 = new ComparablePair<int, string>(1, "one");
 ComparablePair<int, string> pair2 = new ComparablePair<int, string>(1, "two");
@@ -7,7 +7,7 @@ ComparablePair<int, string> pair3 = new ComparablePair<int, string>(1, "one");
 Console.WriteLine(pair1.CompareTo(pair2));
 Console.WriteLine(pair2.CompareTo(pair1));
 Console.WriteLine(pair1.CompareTo(pair3));
-Console.ReadLine();
+
 
 
 
